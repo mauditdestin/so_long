6a6efc21495ba84f8c9b29bae5d00b899493ec59
catlover4242 <akapaui@protonmail.com>
2025-06-07 14:44:32 +0200

    +| rompish

--- checking/map_parsing2.c
+++ checking/map_parsing2.c
@@ -12,7 +12,37 @@
 
 #include "../includes/so_long.h"
 
+static void create_frame(t_data *data, char *buf, int x, int y)
+{
+	while (buf[x] != '\n' && buf[x])
+	{
+		data->map[y][x] = buf[x];
+		data->check_map[y][x] = '0';
+		x++;
+	}
+	data->map[y][data->size_x] = 0;
+	data->check_map[y][data->size_y] = 0;
+}
+
 void map_create(t_data *data, char *filepath)
 {
-	
+	int		y;
+	int		x;
+	int		fd;
+	char	*buf;
+
+	y = 0;
+	fd = open(filepath, O_RDONLY);
+	while (y != data->size_y)
+	{
+		x = 0;
+		buf = get_next_line(fd);
+		check_rectangle(data, buf, fd);
+		create_frame(data, buf, x, y);
+		y++;
+		free(buf);
+	}
+	buf = get_next_line(fd);
+	free(buf);
+	close(fd);
 }
